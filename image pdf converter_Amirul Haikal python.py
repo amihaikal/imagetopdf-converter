72d1378041705b5b9088525c07302b79736db511
amihaikal <108882727+amihaikal@users.noreply.github.com>
2023-08-06 00:54:35 +0800

    Add files via upload

---
+++ image pdf converter_Amirul Haikal python.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox
+from PIL import Image, ImageTk
+
+# Function to convert images to PDF
+def images_to_pdf(images, pdf_name):
+    try:
+        # Create a new PDF file
+        with Image.open(images[0]) as pdf:
+            pdf.save(pdf_name, "PDF", resolution=100.0, save_all=True, append_images=images[1:])
+        messagebox.showinfo("Success", "Images have been successfully converted to PDF.")
+    except Exception as e:
+        messagebox.showerror("Error", f"Failed to convert images to PDF.\nError: {str(e)}")
+
+# Function to select images
+def select_images():
+    images = filedialog.askopenfilenames(title="Select Images", filetypes=(("Image files", "*.jpg;*.jpeg;*.png"),("All files", "*.*")), initialdir="C:/")
+    return images
+
+# Function to select PDF name and path
+def select_pdf():
+    pdf = filedialog.asksaveasfilename(title="Save PDF As", defaultextension=".pdf", initialdir="C:/", filetypes=(("PDF files", "*.pdf"),("All files", "*.*")))
+    return pdf
+
+# Create GUI
+root = tk.Tk()
+root.title("Convert Images to PDF")
+
+select_images_btn = tk.Button(root, text="Select Images", command=select_images)
+select_images_btn.pack()
+
+select_pdf_btn = tk.Button(root, text="Select PDF", command=select_pdf)
+select_pdf_btn.pack()
+
+convert_btn = tk.Button(root, text="Convert", command=lambda: images_to_pdf(select_images(), select_pdf()))
+convert_btn.pack()
+
+root.mainloop()
